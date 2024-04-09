71d14e01d1622a533eaa5002c14833829c3c83bd
Isaiah <isaiah.g.callano@gmail.com>
2024-04-08 17:04:53 -0700

    Import portfolio img

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -2,17 +2,24 @@ import React from "react";
 
 const PortfolioItem = ({ title, imgUrl, stack, link }) => {
     return (
-        <div>
+        <div className="border-2 border-stone-900 rounded-md overflow-hidden">
             <img
                 src={imgUrl}
                 alt="portfolio"
                 className="w-full h-36 md:h-48 object-cover cursor-pointer"
             />
             <div className="w-full p-4">
-                <h3>{title}</h3>
-                <p>
-                    {stack.map(tech => ( 
-                        <span key={tech} className="text-xs bg-gray-200 text-gray-800 p-1 rounded-md mr-1">
+                <h3 className="text-lg med:text-xl mb-2 md:mb-3 font-semibold">
+                    {title}
+                </h3>
+                <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
+                    {stack.map((tech) => (
+                        <span
+                            className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 rounded-md"
+                            key={tech}
+                            // mystery code lol
+                            // className="text-xs bg-gray-200 text-gray-800 p-1 rounded-md mr-1"
+                        >
                             {tech}
                         </span>
                     ))}
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,20 +1,32 @@
 export default [
     {
-        title: "Portfolio Item 1",
-        imgUrl: "",
-        stack: ["HTML", "CSS", "JavaScript"],
+        title: "Song Guesser",
+        imgUrl: "src/assets/song-guesser.png",
+        stack: ["React", "Spotify API"],
         link: "https://www.google.com"
     },
     {
-        title: "Portfolio Item 2",
-        imgUrl: "",
-        stack: ["React", "TailwindCSS", "Vite"],
+        title: "Rental Site Scraper",
+        imgUrl: "src/assets/hasta.png",
+        stack: ["MERN Stack", "US Real Estate API"],
         link: "https://www.google.com"
     },
     {
-        title: "Portfolio Item 3",
-        imgUrl: "",
-        stack: ["Python", "C++", "Robotics"],
+        title: "World Map",
+        imgUrl: "src/assets/world-map.png",
+        stack: ["Angular", "World Bank API"],
+        link: "https://www.google.com"
+    },
+    {
+        title: "Breakout Clone",
+        imgUrl: "src/assets/breakout.png",
+        stack: ["Java", "ObjectDraw Library"],
+        link: "https://www.google.com"
+    },
+    {
+        title: "Markov Text Chain Generator",
+        imgUrl: "src/assets/markov.png",
+        stack: ["Java"],
         link: "https://www.google.com"
     },
 ]
