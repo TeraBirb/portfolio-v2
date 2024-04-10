e07a272d1bd7f22130172f6c8bf7b062618bee63
Isaiah <isaiah.g.callano@gmail.com>
2024-04-09 20:33:00 -0700

    Create footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
 const Footer = () => {
     return (
-        <div className="">
-            <h1>Footer</h1>
+        <div className="py-5 text-center">
+            {/* Insert icons here maybe */}
+            <p className="text-sm mt-2 opacity-50">
+                &copy; {new Date().getFullYear()} Isaiah Callano. All rights reserved.
+            </p>
         </div>
     );
 };
