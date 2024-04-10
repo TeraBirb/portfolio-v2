b833acc44d3b53c330262a0485446824dd66a9c1
Isaiah <isaiah.g.callano@gmail.com>
2024-04-09 20:42:30 -0700

    Initialize dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Intro from "./components/Intro";
 import Portfolio from "./components/Portfolio";
@@ -6,15 +6,25 @@ import Contact from "./components/Contact";
 import Timeline from "./components/Timeline";
 import Footer from "./components/Footer";
 
+const theme = "s";
 function App() {
     return (
-        <div className="App">
-            <Intro />
-            <Portfolio />
-            <Timeline />
-            <Contact />
-            <Footer />
-        </div>
+        <>
+            <button
+                type="button"
+                // onClick={handleThemeSwitch}
+                className="fixed p-2 z-10 right-20 top-4 bg-violet-300 text-lg p-1 rounded-md"
+            >
+                {theme === "light" ? "🌙" : "🌞"}
+            </button>
+            <div className="App">
+                <Intro />
+                <Portfolio />
+                <Timeline />
+                <Contact />
+                <Footer />
+            </div>
+        </>
     );
 }
 
